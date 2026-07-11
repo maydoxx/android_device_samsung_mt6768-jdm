83c13214bb5e6e2ab36f40d9bf9d403c96f85f41
Maydo <261177727+maydoxx@users.noreply.github.com>
2026-07-11 21:58:54 +0300

    Update mt6768.mk

--- mt6768.mk
+++ mt6768.mk
@@ -143,7 +143,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 31
 #$(call inherit-product, vendor/samsung/a05m/a05m-vendor.mk)
 
 # Inherit the sign keys
-$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+$(call inherit-product, vendor/lineage-priv/keys.mk)
 
 # SEC_RIL
 $(call inherit-product, vendor/samsung/secril/sec_ril.mk)
